b53fda9f346d862affd1ec9be04b9544edfd906b
Adeyinka Adegbenro <adeyinkaadegbenro@gmail.com>
2023-05-18 10:49:58 +0100

    Create Redis cache

---
+++ data/mysql-scripts/init.sql
@@ -0,0 +1,20 @@
+GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'redpanda' ;
+
+create database redpanda;
+
+GRANT ALL PRIVILEGES ON redpanda.* TO 'redpanda'@'%';
+
+use redpanda;
+create table leaderboard (
+    id int primary key,
+    username varchar(32),
+    score int unsigned,
+    created_date datetime,
+    last_modified_date datetime
+);
+insert into leaderboard (id, username, score, created_date, last_modified_date)
+values (1, 'curiouscat78', 1000, 'Payment via Net Banking', now(), now());
+insert into leaderboard (id, username, score, created_date, last_modified_date)
+values (2, 'Hokage1998', 3000, 'Payment via Debit Card', now(), now());
+insert into leaderboard (id, username, score, created_date, last_modified_date)
+values (3, 'thanos_ring23', 400, 'Payment via Credit Card', now(), now());
